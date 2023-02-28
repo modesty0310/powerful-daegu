c44d4d956f3203d3c749de19b9f785d4f9025beb
modesty0310 <rlatprua4@gmail.com>
2023-02-28 18:22:32 +0900

    스웨거 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger 추가
+  const config = new DocumentBuilder()
+    .setTitle('대구 아동 급식카드 앱')
+    .setDescription('대구 아동 급식카드 API문서')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
