2a40be67180da5803235643a68c93333507adf89
modesty0310 <rlatprua4@gmail.com>
2023-05-07 13:52:21 +0900

    네이버 api 연동

--- src/maps/maps.controller.ts
+++ src/maps/maps.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import axios from 'axios';
 import { GetDirectionsDto } from './dto/getDirections.dto';
 import { MapsService } from './maps.service';
 
@@ -9,14 +10,29 @@ export class MapsController {
     ) {}
 
     @Get()
-    async getDirections(
-        @Query() query: GetDirectionsDto
-    ) {
-        const {startLat, startLan, goalLat, goalLan} = query
-        const result = await this.mapsService.getDirections(startLat, startLan, goalLat, goalLan);
-        console.log(result);
-        
-        return;
+    async getDirections() {
+        const opt = {
+            url: "https://map.naver.com/v5/api/transit/directions/point-to-point?start=128.582351,35.8642161&goal=128.560192,35.9303298&mode=TIME&lang=ko&includeDetailOperation=true",
+            method: "post",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }
+
+        const result = await axios(opt)
+            .then(function a(response) {
+                console.log("response =", response)
+                return response.data
+                // // 맵 목적지 설정
+                // dispatch(mapActions.handleDestination({data : e.target.value}))
+
+                // // 맵 데이터 설정
+                // dispatch(mapActions.handleSearch({data : response.data.paths.length !== 0 ? response.data.paths : response.data.staticPaths}))
+            })
+            .catch(function (error) {
+                console.log(error);
+                return error
+            });
+            return result
     }
 
 }
