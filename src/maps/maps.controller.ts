987e8193771a159ee6f4ac671dc9f5810b012e02
modesty0310 <rlatprua4@gmail.com>
2023-06-03 17:11:50 +0900

    경로검색 자동차 걷기 추가

--- src/maps/maps.controller.ts
+++ src/maps/maps.controller.ts
@@ -41,4 +41,64 @@ export class MapsController {
             return result
     }
 
+    @Get('car')
+    @ApiOperation({ summary: '경로 가져오기'})
+    async getDirectionsByCar(
+        @Query() query: GetDirectionsDto
+    ) {
+        const {start, goal} = query;
+        const opt = {
+            url: `https://map.naver.com/v5/api/dir/findcar?start=${start}&goal=${goal}&respversion=4&rptype=4&mainoption=traoptimal,avoidhipassonly:traoptimal,multiroute,avoidhipassonly:traoptimal,avoidtoll,avoidhipassonly:traoptdist,avoidhipassonly`,
+            method: "post",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }
+
+        const result = await axios(opt)
+            .then(function a(response) {
+                console.log("response =", response)
+                return response.data
+                // // 맵 목적지 설정
+                // dispatch(mapActions.handleDestination({data : e.target.value}))
+
+                // // 맵 데이터 설정
+                // dispatch(mapActions.handleSearch({data : response.data.paths.length !== 0 ? response.data.paths : response.data.staticPaths}))
+            })
+            .catch(function (error) {
+                console.log(error);
+                return error
+            });
+            return result
+    }
+
+    @Get('walk')
+    @ApiOperation({ summary: '경로 가져오기'})
+    async getDirectionsByWalk(
+        @Query() query: GetDirectionsDto
+    ) {
+        const {start, goal} = query;
+        const opt = {
+            url: `https://map.naver.com/v5/api/dir/findwalk?lo=ko&st=1&o=all&l=${start};${goal}&lang=ko`,
+            method: "post",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }
+
+        const result = await axios(opt)
+            .then(function a(response) {
+                console.log("response =", response)
+                return response.data
+                // // 맵 목적지 설정
+                // dispatch(mapActions.handleDestination({data : e.target.value}))
+
+                // // 맵 데이터 설정
+                // dispatch(mapActions.handleSearch({data : response.data.paths.length !== 0 ? response.data.paths : response.data.staticPaths}))
+            })
+            .catch(function (error) {
+                console.log(error);
+                return error
+            });
+            return result
+    }
+
 }
